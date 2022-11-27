4e4114b556bfca23efe9a8c55a01103c7cb36355
Aleksandr <nastyavin86@br.ru>
2022-11-27 20:56:34 +0300

    Модель для элементов списка и связанная с ней миграция

--- work_log/journal/tests.py
+++ work_log/journal/tests.py
@@ -1,3 +1,4 @@
+from work_log.journal.models import Item
 from django.test import TestCase
 
 
@@ -17,3 +18,25 @@ class HomePageTest(TestCase):
         )
         self.assertIn("Я ничего не делал", response.content.decode())
         self.assertTemplateUsed(response, 'index.html')
+
+
+class RecordModelTest(TestCase):
+    """ Тест модели элемента записи. """
+
+    def test_saving_and_retrieving_items(self):
+        """ Тест: сохранения и получения элементов списка. """
+        first_item = Item()
+        first_item.text = "Я сделал то-то..."
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Что-то делал ..."
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, "Я сделал то-то...")
+        self.assertEqual(second_saved_item.text, "Что-то делал ...")
