301d9a048f5428c69b2012073de6add4efdb4b60
Aleksandr <nastyavin86@br.ru>
2022-11-27 13:04:21 +0300

    Базовое представление теперь возвращает минимальный HTML

--- work_log/journal/tests.py
+++ work_log/journal/tests.py
@@ -1,6 +1,8 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 from work_log.journal.views import home_page
+
 
 class HomePageTest(TestCase):
     """ Тест домашней страницы. """
@@ -8,6 +10,14 @@ class HomePageTest(TestCase):
     def test_root_url_resolve_to_home_page_view(self):
         """ Тест: корневой url преобразуется в представление
             домашней страницы. """
-
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        """ Тест: домашняя страница возвращает правильный html. """
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn("<title>Рабочий журнал</title>", html)
+        self.assertTrue(html.endswith("</html>"))
